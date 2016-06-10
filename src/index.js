7ceb0a66ff0cb672c6e04469c932f61220b64be9
kensplanet <kensplanet@yahoo.com>
2016-06-09 23:08:04 -0400

    Changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBxOvqHwexFvajH0if2ZetjABr8aYeD5mY';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
     return (<div><SearchBar /></div>);
