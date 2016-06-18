59a457d40d8dbbb140a3fc7d3aac227a3b416655
kensplanet <kensplanet@yahoo.com>
2016-06-18 16:08:29 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBxOvqHwexFvajH0if2ZetjABr8aYeD5mY';
 
@@ -19,6 +20,7 @@ class App extends Component {
     render() {
         return (<div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]} />
             <VideoList videos={this.state.videos} />
         </div>);
     };
